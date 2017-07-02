43d614296f46fe556353005955aecc35294bf23d
huyenltnguyen <huyenltnguyen@outlook.com>
2017-07-02 15:11:47 +0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder } from '../actions';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { text: '' };
+		this.state = { 
+			text: '',
+			dueDate: ''
+		};
 	}
 
 	onInputChange = (event) => {
@@ -18,34 +21,60 @@ class App extends Component {
 		this.props.addReminder(this.state.text);
 	}
 
+	deleteReminder = (id) => {
+		this.props.deleteReminder(id);
+	}
+
+	renderReminders = () => {
+		const { reminders } = this.props;
+		return (
+			<ul className="list-group col-sm-4">
+				{
+					reminders.map((reminder) => {
+						return (
+							<li key={reminder.id} className="list-group-item">
+								<div className="list-item">{reminder.text}</div>
+								<div className="list-item delete-button"
+									onClick={ () => this.deleteReminder(reminder.id) }>
+									&#x2715;
+								</div>
+							</li>
+						)
+					})
+				}
+			</ul>
+		);
+	}
+
 	render() {
-		console.log('this.props', this.props);
 		return (
 			<div className="App">
 				<div className="title">Reminder App</div>
-				<div className="form-inline">
+				<div className="form-inline reminder-form">
 					<div className="form-group">
 						<input
 							className="form-control"
 							placeholder="I have to..."
-							onChange={ this.onInputChange } />
-					</div>
-					<div>
-						<button
-							type="button"
-							className="btn btn-success"
-							onClick={ this.addReminder }>
-								Add Reminder
-						</button>
-					</div>
+							onChange={ this.onInputChange }
+						/>
+					</div>					
+
+					<button
+						type="button"
+						className="btn btn-success"
+						onClick={ this.addReminder }>
+							Add Reminder
+					</button>					
 				</div>
+
+				{this.renderReminders()}
 			</div>
 		)
 	}
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({addReminder}, dispatch);
+	return bindActionCreators({addReminder, deleteReminder}, dispatch);
 }
 
 function mapStateToProps(state) {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 const reminder = (action) => {
 	return {
@@ -7,6 +7,12 @@ const reminder = (action) => {
 	}
 }
 
+const removeById = (state = [], id) => {
+	const reminders = state.filter(reminder => reminder.id !== id);
+	console.log('new reduced reminders', reminders);
+	return reminders;
+};
+
 const reminders = (state = [], action) => {
 	let reminders = null;
 
@@ -14,6 +20,9 @@ const reminders = (state = [], action) => {
 		case ADD_REMINDER:
 			reminders = [...state, reminder(action)];
 			console.log('reminders as state', reminders);
+			return reminders;
+		case DELETE_REMINDER:
+			reminders = removeById(state, action.id);
 			return reminders;
 		default:
 			return state;
